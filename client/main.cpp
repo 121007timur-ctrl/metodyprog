cbd6060106bf99f919ace2a15e934545af9ab74e
тимур <121007timur@gmail.com>
2026-05-11 20:11:59 +0300

    Create main.cpp

---
+++ client/main.cpp
@@ -0,0 +1,10 @@
+#include "authregform.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    AUTHregForm w;
+    w.show();
+    return a.exec();
+}

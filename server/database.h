6609be3e00c2e58f0229cce50f5ee4cf438ba762
тимур <121007timur@gmail.com>
2026-05-21 00:36:00 +0300

    Update database.h

--- server/database.h
+++ server/database.h
@@ -3,24 +3,36 @@
 
 #include <QString>
 #include <QMap>
+#include <QVector>
+
+struct GameStatsData {
+    int totalAttempts;
+    int totalCorrect;
+    int gamesPlayed;
+    int bestGameAttempts;
+};
 
 class Database
 {
 public:
     static Database& instance();
 
-
+    // Регистрация и авторизация
     bool registerUser(const QString& email, const QString& login, const QString& password);
     bool loginUser(const QString& loginOrEmail, const QString& password);
     bool isLoggedIn(const QString& loginOrEmail);
-
-
     QString getUserLogin(const QString& email);
     bool userExists(const QString& loginOrEmail);
 
-
+    // Статистика сервера
     int getTotalUsers();
     QVector<QString> getAllLogins();
+
+    // Игровая статистика
+    void updateGameStats(const QString& login, int attempts, bool success);
+    GameStatsData getUserGameStats(const QString& login);
+    void resetUserGameStats(const QString& login);
+    QVector<QPair<QString, int>> getTopPlayers(int limit = 10);
 
 private:
     Database() = default;
@@ -30,6 +42,7 @@ private:
         QString login;
         QString password;
         bool isOnline;
+        GameStatsData gameStats;
     };
 
     QMap<QString, UserData> users;

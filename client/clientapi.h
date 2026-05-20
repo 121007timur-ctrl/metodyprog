bf0f60b3d463897efd5234c148edf3fcf7791ec1
тимур <121007timur@gmail.com>
2026-05-20 22:50:01 +0300

    Update clientapi.h

--- client/clientapi.h
+++ client/clientapi.h
@@ -4,7 +4,6 @@
 #include <QString>
 #include <QTcpSocket>
 
-// Singleton для работы с сервером
 class ClientAPI : public QObject
 {
     Q_OBJECT
@@ -22,22 +21,24 @@ private:
 public:
     static ClientAPI* getInstance();
 
-    // Сеть
     bool connectToServer(const QString& host = "127.0.0.1", int port = 33333);
     void disconnectFromServer();
     bool isConnected() const { return m_connected; }
     QString sendAndReceive(const QString& message, int timeoutMs = 5000);
 
-    // Авторизация
     QString registerUser(const QString& login, const QString& email, const QString& password);
     QString loginUser(const QString& login, const QString& password);
     void logout();
     bool isLoggedIn() const { return m_isLoggedIn; }
     QString getCurrentUser() const { return m_userLogin; }
 
-    // Команды
     QString sendCommand(const QString& command);
     QString getServerStats();
+
+    QString startGame();
+    QString makeGuess(int number);
+    QString getGameStats();
+    QString getHint();
 
 private slots:
     void onConnected();
@@ -45,4 +46,4 @@ private slots:
     void onError(QAbstractSocket::SocketError error);
 };
 
-#endif
+#endif // CLIENTAPI_H

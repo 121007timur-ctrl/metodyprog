7bd19f635051fb376c7d440b22feef28c3544f72
тимур <121007timur@gmail.com>
2026-05-21 00:36:39 +0300

    Update server_functions.h

--- server/server_functions.h
+++ server/server_functions.h
@@ -5,7 +5,6 @@
 #include <QTcpSocket>
 #include <QMap>
 
-
 struct ClientInfo {
     QString clientId;
     QString login;
@@ -15,11 +14,26 @@ struct ClientInfo {
     QString tempLogin;
 };
 
+struct GameSession {
+    int secretNumber;
+    int attempts;
+    int minRange;
+    int maxRange;
+    bool gameActive;
+    int currentNumber;
+};
+
+// Структура для статистики заданий
+struct TaskStats {
+    int totalAttempts;      // Всего решено заданий
+    int correctAnswers;     // Правильных ответов
+};
 
 extern QMap<QTcpSocket*, ClientInfo> g_clients;
 extern QMap<QString, QTcpSocket*> g_logins;
+extern QMap<QTcpSocket*, GameSession> g_gameSessions;
+extern QMap<QTcpSocket*, TaskStats> g_taskStats;  // Статистика заданий
 extern int g_clientCounter;
-
 
 QString generateClientId();
 void sendAuthMenu(QTcpSocket* socket);
@@ -28,11 +42,13 @@ QString handleAuthSteps(QTcpSocket* socket, ClientInfo& info, const QString& req
 QString handleCommands(QTcpSocket* socket, ClientInfo& info, const QString& command);
 void cleanupClient(QTcpSocket* socket);
 
-
 QString getStats();
+QString task1Handler(QTcpSocket* socket, const QString& answer = "");
+QString task2Handler(QTcpSocket* socket, const QString& answer = "");
 
-
-QString task1Handler();
-QString task2Handler();
+QString handleStartGame(QTcpSocket* socket);
+QString handleGuess(QTcpSocket* socket, const QString& guess, const QString& login);
+QString handleGameStats(QTcpSocket* socket, const QString& login);
+QString handleHint(QTcpSocket* socket);
 
 #endif // SERVER_FUNCTIONS_H
